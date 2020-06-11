3d9b44ba620375b46a36c4a033524b2067b1b45c
samitha9125 <samithananayakkara@gmail.com>
2020-06-11 21:40:48 +0530

    Initial Commit

---
+++ precache-manifest.f231a79b14c6ac5eec7ce1223a16b639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c8b6ce7dfd0df067e0d93bca2f03c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "813a5620db30c409cb1f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b9b909c14f3cbde940b9",
+    "url": "/static/js/2.5a93c0c4.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.5a93c0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813a5620db30c409cb1f",
+    "url": "/static/js/main.6840f6b0.chunk.js"
+  },
+  {
+    "revision": "30dad820345466aa2479",
+    "url": "/static/js/runtime-main.d40922cd.js"
+  }
+]);
